238a2ed1e1371a96b1202831f9aeeff893483288
Sri Raksha Siva <108650257+Sri-Raksha@users.noreply.github.com>
2025-10-08 17:05:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Typewriter effect
   const typewriterText = document.querySelector('.typewriter-text');
   const phrases = [
+      "Business Analyst"
       "Artificial Intelligence & Machine Learning Engineer",
       "Data Scientist",
       "Data Analyst",
